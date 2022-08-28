f2025418ef060a0b80117d8cd24b7d9cffaf089f
Augusto Lima <augustolimads@gmail.com>
2022-08-28 10:32:36 -0300

    feature: config prismic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,8 @@
-import Link from 'next/link';
-import { PrismicProvider } from '@prismicio/react';
 import { AppProps } from 'next/app';
-import { PrismicPreview } from '@prismicio/next';
-import { linkResolver, repositoryName } from '../../prismicio.js';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <PrismicProvider
-      linkResolver={linkResolver}
-      internalLinkComponent={({ href, ...props }) => (
-        <Link href={href} passHref>
-          <a {...props} />
-        </Link>
-      )}
-    >
-      <PrismicPreview repositoryName={repositoryName}>
-        <Component {...pageProps} />
-      </PrismicPreview>
-    </PrismicProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
